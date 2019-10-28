06948b312078145c99dd4e4e16e1b76887ac4fbc
KennethMokhethi <kennethmahlatsemokhethi@gmail.com>
2019-10-28 16:27:53 +0200

    Removed the other function

--- src/stringAdd.js
+++ src/stringAdd.js
@@ -1,63 +1,51 @@
-
-// Function that add string numbers
-function strAddTwoArg(a, b) {
-    var sum = 0;
-    var numA = Number(a);
-    var numB = Number(b);
-
-    if ((a != null) && (b != null)) {
-        //summing the two arguments
-        sum = numA + numB;
-
-    } else if ((a != null) && (b == null)) { //return A if B null
-        return numA;
-    } else if ((a == null) && (b != null)) {//return B if A null
-        return numB;
-    } else {
-        return sum;
-    }
-
-    return sum;
-
-}
-
 //Function that accept multiple number of arguments
 function strAddMultArg() {
-    var sum = 0;
-    var numArray = [];
-    var strNum = /-?[0-9]+/g;
+  var sum = 0;
+  var numArray = [];
+  var strNum = /-?[0-9]+/g;
 
-    //Extracting numbers from the string
-    for (let i = 0; i < arguments.length; i++) {
-        numArray[i] = arguments[i].match(strNum).map(Number);
-    }
-
-    //Error handling for negative numbers
-    try {
-        for (let c = 0; c < numArray[0].length; c++) {
-
-            if (numArray[0][c] < 0) throw "number negative :" + numArray[0][c];
+  if (arguments[0] != null) {
+    if (arguments[1] != null || arguments.length === 1) {
+      //Extracting numbers from the string
+      let twoDarry = [];
+      for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i].match(strNum).map(Number).length > 1) {
+          for (
+            let c = 0;
+            c < arguments[i].match(strNum).map(Number).length;
+            c++
+          ) {
+            twoDarry[c] = arguments[i].match(strNum).map(Number);
+            numArray[c] = twoDarry[i][c]; /////split
+          }
+        } else {
+          numArray[i] = arguments[i];
         }
+      }
 
-    } catch (err) {
+      //Error handling for negative numbers
+      try {
+        for (let c = 0; c < numArray.length; c++) {
+          if (numArray[c] < 0) throw "number negative :" + numArray[c];
+        }
+      } catch (err) {
         return "Error:" + err;
-    }
-
-    //Summing the arguments
-    for (let c = 0; c < numArray[0].length; c++) {
-
+      }
+      console.log(numArray);
+      //Summing the arguments
+      for (let c = 0; c < numArray.length; c++) {
         //Ignoring numbers that are greater than 1000
-        if (numArray[0][c] > 1000) {
-            numArray[0][c] = 0;
+        if (numArray[c] > 1000) {
+          numArray[c] = 0;
         }
-        sum += numArray[0][c];
+        sum += Number(numArray[c]);
+      }
+    } else {
+      sum = Number(arguments[0]);
     }
+  } else {
+    sum = Number(arguments[1]);
+  }
 
-    return sum;
-
+  return sum;
 }
-
-
-
-
-
